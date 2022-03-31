e8390a1f9f4f74c22c0cd9696680791f1b0a693a
Marcus Hill <hillmarcus32@gmail.com>
2022-03-31 01:14:56 -0400

    Used react props to create todo list items and react state with callback handler to display paragraph contining submitted title input value

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,30 @@
 import React from "react";
-
-const todoList = [
-  {
-    title: "Complete Assignment 1.1",
-    id: 0
-  },
-  {
-    title: "Attend at least 2 mentor session this week",
-    id: 1
-  },
-  {
-    title: "Complete your daily workout",
-    id: 2
-  }
-]
+import TodoListItem from "./TodoListItem";
 
 function TodoList () {
+
+  const todoList = [
+    {
+      title: "Complete Assignment 1.1",
+      id: 0
+    },
+    {
+      title: "Attend at least 2 mentor session this week",
+      id: 1
+    },
+    {
+      title: "Complete your daily workout",
+      id: 2
+    }
+  ];
+
   return (
     <ul>
       {todoList.map(function(item){
-        return <li key = {item.id}>{item.title}</li>
+        return <TodoListItem key={item.id} item={item}/>
       })}
     </ul>
   );
-}
+};
 
 export default TodoList;
---
+++ src/TodoListItem.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+function TodoListItem (props){
+  return(
+    <li>{props.item.title}</li>
+  );
+};
+
+export default TodoListItem;
