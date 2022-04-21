3f068ad49331778410f655914c9a725d9ad3afd9
Marcus Hill <hillmarcus32@gmail.com>
2022-04-20 22:35:46 -0400

    Updating comments to describe commited changes

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,28 +1,32 @@
 import React from "react";
 /*
-Create function component AddTodoForm that accepts props found during instantization.
+Create function component AddTodoForm that accepts destructured properties found during instantization.
 AddTodoForm returns a form that allows users to add a todo title to their todo list.
 */
 function AddTodoForm ({ onAddTodo }){
-
+  /*
+  Call and destructure React.useState hook to set todoTitle variable with value defined by setTodoTitle function
+  */
+  const [todoTitle, setTodoTitle] = React.useState("");
+  /*
+  Create a handler function handleTitleChange that accepts event values in response to the input onChange event
+  handleTitleChange creates newTodoTitle variable that is equal to the current input value
+  handleTitleChange uses setTodoTitle functon to update todoTitle with value stored in newTodoTilte
+  */
   function handleTitleChange (e){
     const newTodoTilte = e.target.value;
     setTodoTitle(newTodoTilte);
   }
   /*
-  Create a handler function handleAddTodo that accepts event values in response to the onSubmit event.
+  Create a handler function handleAddTodo that accepts event values in response to the form onSubmit event.
   handleAddTodo will prevent reloading on submission
-  handleAddTodo will pass the user input value into todoTitle variable
-  todoTitle variable is passed into onAddTodo property which provides the setNewTodo argument used in React.useState()
+  todoTitle variable is passed into onAddTodo property used as an addTodo argument found in App component.
   */
   function handleAddTodo (e){
     e.preventDefault();
     onAddTodo({title: todoTitle, id: Date.now()});
     setTodoTitle("");
-    console.log(todoTitle);
   };
-
-  const [todoTitle, setTodoTitle] = React.useState("");
 
   return(
     /*
--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 /*
 Create main function component App that returns a container holding all other function components and HTML JSX
-returns heading title "Todo List", AddTodoForm component with onAddTodo handler, paragraph displaying user input, and TodoList component
+returns heading title "Todo List", AddTodoForm component with onAddTodo handler, and TodoList component with todoList property
 */
 function App() {
   /*
-  Call and destructure React.useState hook to load paragraph value defined as newTodo and update value through setNewTodo function
+  Call and destructure React.useState hook to set todoList variable with value defined by setTodoList function.
+  todoList is initialized as empty array
   */
   const [todoList, setTodoList] = React.useState([]);
-
+  /*
+  addTodo function accepts newTodo parameter and calls setTodoList setter function which accepts a new array with spreaded todoList items and newTodo item.
+  */
   function addTodo (newTodo){
     setTodoList([...todoList, newTodo]);
   }
@@ -19,7 +22,7 @@ function App() {
     <div>
       <h1>Todo List</h1>
       {/*
-      onAddTodo property of AddTodoForm component works as a callback handler function to define setNewTodo value based on user input within AddTodoForm input feild.
+      onAddTodo property of AddTodoForm component works as a callback handler function to define newTodo in addTodo function based on user input within AddTodoForm input feild.
       */}
       <AddTodoForm onAddTodo={addTodo}/>
       <TodoList todoList={todoList}/>
