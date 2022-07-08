c75da356ddfc27cc39a92560850ca205da37c9c1
Marcus Hill <hillmarcus32@gmail.com>
2022-07-08 16:09:22 -0400

    Added components file and propType testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import style from "./App.module.css";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
@@ -86,7 +87,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={
           <React.Fragment>
-            <h1>Todo List</h1>
+            <h1 className={style.title}>Todo List</h1>
             {/*onAddTodo property of AddTodoForm component works as a callback handler to define newTodo in addTodo function based on user input within AddTodoForm input feild.*/}
             <AddTodoForm onAddTodo={addTodo}/>
             {/*
