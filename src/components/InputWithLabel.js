e3a881f894f7063504592a8e165e48765e8a3bd7
Marcus Hill <hillmarcus32@gmail.com>
2022-07-09 13:17:18 -0400

    PropTypes and Components folder

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,5 +1,17 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import style from "./InputWithLabel.module.css"
+
+InputWithLabel.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  children: PropTypes.string,
+}
+
+
 /*
 This react component returns a label and input element and is created to be reusable by accessing props declared at instantization
 */
